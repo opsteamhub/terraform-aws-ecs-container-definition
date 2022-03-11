28103d82563ddeecd9d366324549b99e1fbbf0f3
rafael <rafaelviniciuscardoso@gmail.com>
2022-03-11 18:51:09 -0300

    replace environment to container_environment

--- main.tf
+++ main.tf
@@ -83,7 +83,7 @@ locals {
     memory                 = var.container_memory
     memoryReservation      = var.container_memory_reservation
     cpu                    = var.container_cpu
-    container_environment  = local.final_environment_vars
+    environment_container  = local.final_environment_vars
     environmentFiles       = var.environment_files
     secrets                = local.final_secrets_vars
     dockerLabels           = var.docker_labels
